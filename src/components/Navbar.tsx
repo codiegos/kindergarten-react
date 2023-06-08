74a2ac44a485b47675b8428ad923f557dd994c47
Diego Rivera <diegosebsk@gmail.com>
2023-06-07 20:14:08 -0400

    add comments to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,18 +14,21 @@ const Navbar = () => {
   const [closeMenu, setCloseMenu] = useState(false)
 
   return (
-    <header className='fixed bg-white z-20 w-full lg:py-4 px-4'>
+    <header className='fixed bg-white z-20 w-full lg:py-4 px-4 shadow-md'>
       <div className='flex items-center justify-between'>
+        {/* BEGIN Logo */}
         <nav className='flex flex-grow basis-0 z-20'>
-          <a href='#'>
+          <a href='#inicio'>
             <img
               src={logo}
               alt='logo-jardin-las-ardillitas'
-              className='h-12 w-full lg:h-12 cursor-pointer'
+              className='h-12 w-full lg:h-12 cursor-pointer brightness-125'
             />
           </a>
         </nav>
-        <div className='flex items-center justify-end'>
+        {/* END Logo */}
+        <div className='flex items-center'>
+          {/* BEGIN Responsive Hamburger */}
           <input
             type='checkbox'
             name='hamburger'
@@ -50,14 +53,12 @@ const Navbar = () => {
               className='m-auto mt-2 h-0.5 w-6 rounded bg-teal-500 transition duration-300'
             />
           </label>
-
-          <div
-            className={
-              'peer-checked:translate-x-0 translate-x-[-136%] fixed inset-0 w-4/6 text-2xl bg-teal-400/95 lg:bg-transparent transition duration-500 lg:text-xl lg:w-auto lg:static lg:shadow-none lg:translate-x-0'
-            }
-          >
+          {/* END Responsive Hamburger */}
+          {/* BEGIN Sidebar */}
+          <div className='peer-checked:translate-x-0 translate-x-[-136%] fixed inset-0 w-4/6 text-2xl bg-teal-200/95 lg:bg-transparent transition duration-500 lg:text-xl lg:w-auto lg:static lg:shadow-none lg:translate-x-0'>
             <div className='flex flex-col h-full justify-between lg:items-center lg:flex-row'>
-              <ul className='px-6 pt-32  space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0'>
+              {/* BEGIN Nav items */}
+              <ul className='px-6 pt-32 space-y-8 md:px-12 lg:space-y-0 lg:flex lg:space-x-12 lg:pt-0'>
                 {navItems.map(({ key, label, link }) => (
                   <li key={key}>
                     <a
@@ -74,6 +75,9 @@ const Navbar = () => {
                   </li>
                 ))}
               </ul>
+              {/* END Nav items */}
+
+              {/* BEGIN Responsive Social media */}
               <nav className='flex gap-3 items-center px-6 border-t-2 border-teal-500 py-8 lg:hidden'>
                 <a
                   href='https://www.facebook.com/profile.php?id=100088444905927'
@@ -92,9 +96,14 @@ const Navbar = () => {
                   <InstagramIcon />
                 </a>
               </nav>
+              {/* END Responsive Social media */}
             </div>
           </div>
+          {/* END Sidebar */}
         </div>
+        {/* END Responsive Hamburger */}
+
+        {/* BEGIN Social media */}
         <nav className='hidden lg:flex gap-3 items-center flex-grow basis-0 justify-end'>
           <a
             href='https://www.instagram.com/jardin_elcastillo'
@@ -113,6 +122,7 @@ const Navbar = () => {
             <FacebookIcon />
           </a>
         </nav>
+        {/* END Social media */}
       </div>
     </header>
   )
