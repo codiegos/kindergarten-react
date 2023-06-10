14d03ed6c6e2614221c65197de093d5574ac15a0
Diego Rivera <diegosebsk@gmail.com>
2023-06-10 19:28:33 -0400

    add icons to schedule section

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -1,13 +1,22 @@
+import AboutCard from './AboutCard'
+import { CalendarIcon, LevelIcon } from './Icons'
+
 function Schedule () {
   return (
     <section className='bg-gradient-to-b to-15% from-teal-50 to-teal-50 flex flex-col items-center py-24 sm:min-h-screen sm:py-32'>
       <div className='container flex flex-col items-center justify-center gap-8 xl:gap-12'>
-        <h1 className='text-3xl lg:text-6xl text-center'>HORARIOS</h1>
-        <div className='flex flex-col lg:flex-row gap-8 lg:gap-16 items-start justify-center w-full max-w-6xl mt-16'>
-          <div className='flex flex-col gap-4 items-center justify-center bg-white rounded border p-4 shadow-md max-w-md'>
-            <h2 className='inline-flex items-center gap-2 text-2xl lg:text-4xl'>
-              Nuestra Misión
-            </h2>
+        <h1 className='text-3xl sm:text-4xl lg:text-6xl text-center'>
+          HORARIOS
+        </h1>
+        <div className='grid grid-cols-1 sm:grid-cols-2 gap-6'>
+          <AboutCard
+            title='Jornadas de Atención'
+            icon={
+              <CalendarIcon className='w-16 h-16 bg-teal-300 rounded-full' />
+            }
+            colSpan='1'
+            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, qut quibusdam, sapiente sint tempora magni explicabo exercitationem veritatis rem repudiandae error ullam molestiae, doloremque perspiciatis sed abab.'
+          >
             <p className='text-lg lg:text-xl text-justify'>
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
               voluptatum, quibusdam, voluptates, quia quos voluptate quod
@@ -17,17 +26,12 @@ function Schedule () {
               dolorem sed perspiciatis exercitationem neque autem doloribus
               corporis, quis dolores iure. Quae nostrum ipsam tenetur rem?
             </p>
-          </div>
-          <div className='flex flex-col gap-4 items-center justify-center bg-white rounded border p-4 shadow-md max-w-md'>
-            <h2 className='inline-flex items-center gap-2 text-2xl lg:text-4xl'>
-              Nuestra Visión
-            </h2>
-            <p className='text-lg lg:text-xl text-justify'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-              voluptatum, quibusdam, voluptates, quia quos voluptate quod
-              voluptatibus quas quibusdam, voluptates, quia quos voluptate quod
-            </p>
-          </div>
+          </AboutCard>
+          <AboutCard
+            title='Niveles'
+            icon={<LevelIcon className='w-16 h-16 bg-amber-300 rounded-full' />}
+            text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam voluptatum, qut quibusdam, sapiente sint tempora magni explicabo exercitationem veritatis rem repudiandae error ullam molestiae, doloremque perspiciatis sed ab. ab. ab. ab. ab.'
+          />
         </div>
       </div>
     </section>
